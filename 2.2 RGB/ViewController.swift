a5f4bfac034609cf06eba954cf64f9e0588d245e
Moshkin <Moshkin>
2021-10-24 13:34:57 +0300

    Немного поправишь изначальные настройки

--- 2.2 RGB/ViewController.swift
+++ 2.2 RGB/ViewController.swift
@@ -18,21 +18,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLabel.text = "125"
-        greenLabel.text = "125"
-        blueLaabel.text = "125"
         redSlider.value = 125
         greenSlider.value = 125
         blueSlider.value = 125
- //       view.backgroundColor = CGColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1);)
-//            .withAlphaComponent(CGFloat(slider.value))
+        redLabel.text = "\(Int(redSlider.value)) = (\(redSlider.value/255))"
+        greenLabel.text = "\(Int(greenSlider.value)) = (\(greenSlider.value/255))"
+        blueLaabel.text = "\(Int(blueSlider.value)) = (\(blueSlider.value/255))"
+        view.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value/255),
+            green: CGFloat(greenSlider.value/255),
+            blue: CGFloat(blueSlider.value/255), alpha: 1)
     }
     @IBAction func sliderChange(_ sender: UISlider) {
 //        view.backgroundColor = view.backgroundColor?.cgColor(greenSlider.value)
     }
     
     @IBAction func redSliderAction() {
-        redLabel.text = String(Int(redSlider.value))
+        redLabel.text = String("\(Int(redSlider.value)) = (\(redSlider.value/255))")
         view.backgroundColor = UIColor(
             red: CGFloat(redSlider.value/255),
             green: CGFloat(greenSlider.value/255),
@@ -40,7 +42,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func greenSliderAction() {
-        greenLabel.text = String(Int(greenSlider.value))
+        greenLabel.text = String("\(Int(greenSlider.value)) = (\(greenSlider.value/255))")
         view.backgroundColor = UIColor(
             red: CGFloat(redSlider.value/255),
             green: CGFloat(greenSlider.value/255),
@@ -48,7 +50,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func blueSliderAction() {
-        blueLaabel.text = String(Int(blueSlider.value))
+        blueLaabel.text = String("\(Int(blueSlider.value)) = (\(blueSlider.value/255))")
         view.backgroundColor = UIColor(
             red: CGFloat(redSlider.value/255),
             green: CGFloat(greenSlider.value/255),
