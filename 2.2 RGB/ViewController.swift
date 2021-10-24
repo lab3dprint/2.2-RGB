0170d6c762d925137e0f5bc085318c58e7bea791
Moshkin <Moshkin>
2021-10-24 11:02:01 +0300

    Initial Commit

---
+++ 2.2 RGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.2 RGB
+//
+//  Created by Moshkin Ivan on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
